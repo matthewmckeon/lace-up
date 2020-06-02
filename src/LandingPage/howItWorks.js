3bfeade9d7360ca6d7e7b8868adc68e8214d1be6
Ann Hoang <lph2yr@virginia.edu>
2020-06-02 15:00:11 -0400

    set up complete

---
+++ src/LandingPage/howItWorks.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class howItWorks extends Component {
+    render() {
+        return (
+            <div>
+                Example landing page
+            </div>
+        )
+    }
+}
