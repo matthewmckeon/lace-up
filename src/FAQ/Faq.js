5ac4b932cdf8b6805bb61cfdc0971db3e79ce601
Ann Hoang <lph2yr@virginia.edu>
2020-06-03 17:00:18 -0400

    finished faq

--- src/FAQ/Faq.js
+++ src/FAQ/Faq.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
 import SimpleExpansionPanel from './SimpleExpansionPanel.js'
+import { REFERRAL_QS } from "./referralQs.js";
+import { LACEUP_QS } from "./laceUpQs.js";
 
 export default class Faq extends Component {
     render() {
-        console.log(this.props)
         return (
-            <SimpleExpansionPanel />
+            <div>
+                <a name="top"></a>
+                <h1>LaceUp FAQs</h1>
+                <a href="#referralTop">Referral Program</a>
+                <br />
+                <a href="#laceupTop">LaceUp</a>
+
+                <a name="referralTop"></a>
+                <h2>Questions About Referral Program</h2>
+                {REFERRAL_QS.map((qa, index) => {
+                    return (
+                        <SimpleExpansionPanel key={index} qa={qa} qn={index} />
+                    )
+                }
+                )}
+
+                <a name="laceupTop"></a>
+                <h2>Questions About Lace Up</h2>
+                {LACEUP_QS.map((qa, index) => {
+                    return (
+                        <SimpleExpansionPanel key={index} qa={qa} qn={index} />
+                    )
+                }
+                )}
+                <a href="#top">Back to top</a>
+            </div>
         )
+
     }
 }
 
---
+++ src/FAQ/laceUpQs.js
@@ -0,0 +1,39 @@
+export const LACEUP_QS = [
+    {
+        q: "What is LaceUp?",
+        a: "Our company LaceUp will be an online based shoe warehouse. We specifically specializes in sneakers and designing them for the most comfort in daily wear and sports wear. We have several unique services that you cannot find anywhere else. Firstly, we have a shoe prepare program where you can send us your shoes if there are any damages (terms and conditions can be seen on the home page). However, we want to emphasize that we are environmentally conscious as well. One of our services include collecting old shoes,then recycle them or refurbrish them. Furthermore, we also want to contribute more to our community by donating one pair of shoes for every pair purchased to charities. We are aware that there are so many shoes that are being thrown away because they are simply too old. Here, we will refurbrish and donate our shoes to those in need. More information can be found on our website!"
+    },
+    {
+        q: "What kind of shoes do you sell?",
+        a: "We are producing  comfortable sneakers for a variety of situations, whether you are strolling with your family, or wrestling with your dog, or running up mountains, we are there for you! Please go to our main website and browse through our selection!"
+    },
+    {
+        q: "Do I need to sign up for an account to purchase your products?",
+        a: "No, you do not have to. When you check out, please be sure to save your receipt number. However, we highly recommend you register with our referral program for surprising prizes and exclusive updates on our shoes!"
+    },
+    {
+        q: "What services do you offer?",
+        a: "All information can be found on our referral pages under 'About Us'"
+    },
+    {
+        q: "Is there free return or shipping if my shoes don't fit when they arrive?",
+        a: "We offer a 30 day free shipping for returns from the day of purchase. You can also quickly opt in to exchange for another size before you return your previous purchase! However, please read the terms and conditions carefully. We do not take back purposefully damaged shoes or accept any returns after 30 days. However, you many donate them back to us so that we may recycle or refurbrish them"
+    },
+    {
+        q: "My shoes arrived not in perfect condition, will I get a refund?",
+        a: "We will refund your purchase and pay for shipping to return them to us. You may also exchange for another pair and we will investigate and make sure that your subsequent pairs of shoes will never arrive like that anymore!"
+    },
+    {
+        q: "My pet ate my shoe, do I get a refund?",
+        a: "Unfortunately, we cannot refund for those situations. However, you can always send the shoes to us for prepare at 50% off for your first prepare by us!"
+    },
+    {
+        q: "How long is shipping?",
+        a: "Shipping will take about 3-10 business days, depending on your location!"
+    },
+    {
+        q: "How do I contact customer service?",
+        a: "Please email us at laceup@fakeemail.com and our phone number is 555-555-5555. All of our contact information is available on our main website!"
+    }
+
+]
