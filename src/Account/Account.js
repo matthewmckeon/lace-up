2115ac788390524e08f796472c0a091cb4fdd6b9
Ann Hoang <lph2yr@virginia.edu>
2020-06-04 19:39:42 -0400

    protected user account

--- src/Account/Account.js
+++ src/Account/Account.js
@@ -6,68 +6,65 @@ import ReferralHistory from "./ReferralHistory";
 import FriendButton from "./FriendButton.js";
 import Milestones from "./Milestones.js";
 import "./Account.css";
-
-import firebase from 'firebase';
-import { base } from '../config/Firebase';
 import { BrowserRouter as Redirect } from "react-router-dom";
+import firebase from 'firebase';
+import { base } from "../config/Firebase";
 
-export default class Account extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      progress: 0,
-      mileStoneHoverIndex: -1,
-      isLoggedIn: false
-    };
-  }
+export default function Account(props) {
 
-  componentWillMount() {
-    if (base.initializedApp.auth().currentUser) {
-      this.setState({ isLoggedIn: true })
-      let userCode = this.props.match.params.userId;
+  const [progress, setProgress] = React.useState(0)
+  const [mileStoneHoverIndex, setMileStone] = React.useState(-1)
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false)
 
-      let referralLink = "/users/" + userCode
+  React.useEffect(() => {
+    let userCode = props.match.params.userId;
 
-      let referredRef = firebase.database().ref(referralLink)
-      referredRef.once('value', snap => {
-        if (snap.val()) {
-          this.setState({
-            progress: snap.val().score
-          })
-        }
-      })
-    } else {
-      this.setState({ isLoggedIn: false })
-      this.props.history.push("/login")
-    }
+    let referralLink = "/users/" + userCode
 
-  };
+    let referredRef = firebase.database().ref(referralLink)
+    referredRef.once('value', snap => {
+      if (snap.val()) {
+        setProgress(snap.val().score)
+      }
+    })
+
+    let unsub = base.initializedApp.auth().onAuthStateChanged(function (user) {
+      if (user) {
+        setIsLoggedIn(true)
+        console.log(true)
+      }
+      else {
+        setIsLoggedIn(false)
+        console.log(false)
+        props.history.push("/login")
+      }
+    })
 
 
-  render() {
+  }, [])
 
-    return (
-      (this.state.isLoggedIn) ?
 
-        <div className="account">
-          <div className="left">
+  return (
+    (isLoggedIn) ?
+      <div className="account">
+        <div className="left">
 
-            <div className="welcome">
-              <h3 style={{ textTransform: "capitalize", fontSize: "40px", textAlign: "center" }}>
-                Welcome {this.props.match.params.firstName}!
+          <div className="welcome">
+            <h3 style={{ textTransform: "capitalize", fontSize: "40px", textAlign: "center" }}>
+              Welcome {props.match.params.firstName}!
             </h3>
-            </div>
-            <Progress progress={this.state.progress} />
-            <ProgressStats progress={this.state.progress} />
-            <div className="refer-button">
-              <FriendButton code={this.props.match.params.userId} />
-            </div>
-            <ReferralHistory users={this.props.users} currentCode={this.props.match.params.userId} />
           </div>
-
+          <Progress progress={progress} />
+          <ProgressStats progress={progress} />
+          <div className="refer-button">
+            <FriendButton code={props.match.params.userId} />
+          </div>
+          <ReferralHistory users={props.users} currentCode={props.match.params.userId} />
         </div>
-        :
-        <Redirect to="/login" />
-    );
-  }
+
+      </div>
+      :
+      <Redirect to="/login" />
+  );
+
 }
