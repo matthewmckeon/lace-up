851664b55892ec796d445aea28e5c09be70e391d
Alex Kyereboah <atk4kcw@virginia.edu>
2020-06-04 17:48:47 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lace-up/precache-manifest.4688137f1e03e15360ea1e4cce3cfa65.js"
+  "/lace-up/precache-manifest.2c8d3406d1579c208f4fbbe7853838f5.js"
 );
 
 self.addEventListener('message', (event) => {
