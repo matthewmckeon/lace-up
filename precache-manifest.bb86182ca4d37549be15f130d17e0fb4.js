1d409a3ee17f52a0356b555e0305f4397e501802
Alex Kyereboah <atk4kcw@virginia.edu>
2020-06-04 08:49:45 -0400

    Updates

---
+++ precache-manifest.bb86182ca4d37549be15f130d17e0fb4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2a581a9ed70ed65e14c5364b7fbdebf",
+    "url": "/lace-up/index.html"
+  },
+  {
+    "revision": "6edc611f52575521339a",
+    "url": "/lace-up/static/css/2.52f2db83.chunk.css"
+  },
+  {
+    "revision": "0491b51909b321f57367",
+    "url": "/lace-up/static/css/main.8eb8d932.chunk.css"
+  },
+  {
+    "revision": "6edc611f52575521339a",
+    "url": "/lace-up/static/js/2.be9898a7.chunk.js"
+  },
+  {
+    "revision": "fbfb5e42a41e7004782949e93fdf296a",
+    "url": "/lace-up/static/js/2.be9898a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0491b51909b321f57367",
+    "url": "/lace-up/static/js/main.7ed9e51b.chunk.js"
+  },
+  {
+    "revision": "348261dd885cd63d2e7a",
+    "url": "/lace-up/static/js/runtime-main.19010777.js"
+  }
+]);
