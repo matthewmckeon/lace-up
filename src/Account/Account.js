33374d66941eb0534b4347a306a1cc22d25f1f1c
Ann Hoang <lph2yr@virginia.edu>
2020-06-04 19:22:01 -0400

    protect user link

--- src/Account/Account.js
+++ src/Account/Account.js
@@ -8,6 +8,8 @@ import Milestones from "./Milestones.js";
 import "./Account.css";
 
 import firebase from 'firebase';
+import { base } from '../config/Firebase';
+import { BrowserRouter as Redirect } from "react-router-dom";
 
 export default class Account extends Component {
   constructor(props) {
@@ -15,43 +17,57 @@ export default class Account extends Component {
     this.state = {
       progress: 0,
       mileStoneHoverIndex: -1,
+      isLoggedIn: false
     };
   }
 
   componentWillMount() {
-    let userCode = this.props.match.params.userId;
+    if (base.initializedApp.auth().currentUser) {
+      this.setState({ isLoggedIn: true })
+      let userCode = this.props.match.params.userId;
 
-    let referralLink = "/users/" + userCode
+      let referralLink = "/users/" + userCode
 
-    let referredRef = firebase.database().ref(referralLink)
-    referredRef.once('value', snap => {
-      if (snap.val()) {
-        this.setState({
-          progress: snap.val().score
-        })
-      }
-    })
+      let referredRef = firebase.database().ref(referralLink)
+      referredRef.once('value', snap => {
+        if (snap.val()) {
+          this.setState({
+            progress: snap.val().score
+          })
+        }
+      })
+    } else {
+      this.setState({ isLoggedIn: false })
+      this.props.history.push("/login")
+    }
+
   };
 
+
   render() {
+
     return (
-      <div className="account">
-        <div className="left">
+      (this.state.isLoggedIn) ?
 
-          <div className="welcome">
-            <h3 style={{ textTransform: "capitalize", fontSize: "40px", textAlign: "center" }}>
-              Welcome {this.props.match.params.firstName}!
+        <div className="account">
+          <div className="left">
+
+            <div className="welcome">
+              <h3 style={{ textTransform: "capitalize", fontSize: "40px", textAlign: "center" }}>
+                Welcome {this.props.match.params.firstName}!
             </h3>
+            </div>
+            <Progress progress={this.state.progress} />
+            <ProgressStats progress={this.state.progress} />
+            <div className="refer-button">
+              <FriendButton code={this.props.match.params.userId} />
+            </div>
+            <ReferralHistory users={this.props.users} currentCode={this.props.match.params.userId} />
           </div>
-          <Progress progress={this.state.progress} />
-          <ProgressStats progress={this.state.progress} />
-          <div className="refer-button">
-            <FriendButton code={this.props.match.params.userId} />
-          </div>
-          <ReferralHistory users={this.props.users} currentCode={this.props.match.params.userId} />
-        </div>
 
-      </div>
+        </div>
+        :
+        <Redirect to="/login" />
     );
   }
 }
