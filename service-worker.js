54ca8fa54142ad8a104ab11468c71aee1e1c7d85
Matt Thompson <mattluthompson@gmail.com>
2020-06-05 11:07:18 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lace-up/precache-manifest.c39ef99c81a1acc8c524e370d915d713.js"
+  "/lace-up/precache-manifest.438e8212b8cef5f697c963febe0da5a4.js"
 );
 
 self.addEventListener('message', (event) => {
