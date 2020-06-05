ddb2f49a9eb04512f6393059d1286ead9f4c1d8e
Ann Hoang <lph2yr@virginia.edu>
2020-06-04 20:05:05 -0400

    no autoplay

--- src/LandingPage/Card.js
+++ src/LandingPage/Card.js
@@ -35,12 +35,12 @@ export default function SimpleCard(props) {
                 <h1 style={{ position: "relative", left: "5vw" }}>{props.rule.title}</h1>
                 <p>{props.rule.descr}</p>
                 {props.rule.title.includes("Sign Up") ? (
-                    <video style={{ position: "relative", top: "7vh", width: "100%", height: "auto" }} controls autoplay loop><source src={require('../images/register-vid.mp4')} type="video/mp4" /></video>
+                    <video style={{ position: "relative", top: "7vh", width: "100%", height: "auto" }} controls loop><source src={require('../images/register-vid.mp4')} type="video/mp4" /></video>
 
                 ) : props.rule.title === "Get Your Code!" ? (
-                    <video style={{ width: "100%", height: "auto" }} controls autoplay loop><source src={require('../images/withCode.mp4')} type="video/mp4" /></video>
+                    <video style={{ width: "100%", height: "auto" }} controls loop><source src={require('../images/withCode.mp4')} type="video/mp4" /></video>
                 ) : (
-                            <video style={{ position: "relative", top: "7vh", width: "100%", height: "auto" }} controls autoplay loop><source src={require('../images/progressJohn.mp4')} type="video/mp4" /></video>
+                            <video style={{ position: "relative", top: "7vh", width: "100%", height: "auto" }} controls loop><source src={require('../images/progressJohn.mp4')} type="video/mp4" /></video>
                         )}
             </CardContent>
         </Card >
