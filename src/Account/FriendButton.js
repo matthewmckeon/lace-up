3522bf78e19f359926771a6a390c36dd63e13273
Matt Thompson <mattluthompson@gmail.com>
2020-06-05 12:08:51 -0400

    adding fb and twitter icons

--- src/Account/FriendButton.js
+++ src/Account/FriendButton.js
@@ -5,6 +5,7 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import CopyIcon from "@material-ui/icons/FileCopy";
+import { FacebookIcon, TwitterIcon, FacebookShareButton, TwitterShareButton } from "react-share";
 import "./FriendButton.css";
 
 export default function FriendButton(props) {
@@ -57,7 +58,21 @@ export default function FriendButton(props) {
             >
               <CopyIcon />
             </IconButton>
+            
           </div>
+          <div className="referral">
+            <FacebookShareButton 
+              quote={"Want cool prizes like a free pair of shoes? Lace-Up is a great company that has an amazing social impact, environmental values, and a cool community of users!  Sign up today using my referral code: "+props.code}
+              url="https://mattluthompson.github.io/lace-up/#/lace-up/" 
+              children={<div style = {{display: "flex", textAlign:"center"}}><FacebookIcon size={30} style={{position: "relative", top:"10px"}}/><p style={{marginLeft:"10px"}}>Share on Facebook!</p></div>}/>
+          </div>
+          <div className="referral">
+            <TwitterShareButton 
+              title={"Want cool prizes like a free pair of shoes? Lace-Up is a great company that has an amazing social impact, environmental values, and a cool community of users!  Sign up today using my referral code: "+props.code}
+              via="laceupshoes_io"
+              url="https://mattluthompson.github.io/lace-up/#/lace-up/" 
+              children={<div style = {{display: "flex", textAlign:"center"}}><TwitterIcon size={30} style={{position: "relative", top:"10px"}}/><p style={{marginLeft:"10px"}}>Share on Twitter!</p></div>}/>
+            </div>
         </div>
       </Popover>
     </div>
